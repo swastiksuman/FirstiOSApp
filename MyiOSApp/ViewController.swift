e3751bcce311ef131a90d309ad8af3b15f8e37db
Swastik Panda <swastiksuman@gmail.com>
2020-03-28 20:39:24 +0530

    added table view

--- MyiOSApp/ViewController.swift
+++ MyiOSApp/ViewController.swift
@@ -8,7 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let dailyTasks = ["Wake Up", "Excercise", "Breakfast", "Code"]
+    
+    let weeklyTasks = ["Grocery", "Tennis"]
+    
+    let monthlyTasks = ["Electricity Bill", "House Rent", "Telephone Bill"]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section{
+        case 0:
+            return dailyTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return monthlyTasks.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        switch indexPath.section{
+        case 0:
+            cell.textLabel?.text = dailyTasks[indexPath.row]
+        case 1:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        case 2:
+            cell.textLabel?.text=monthlyTasks[indexPath.row]
+        default:
+            cell.textLabel?.text = "Error!"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section{
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "Monthly Tasks"
+        default:
+            return "Error!"
+        }
+    }
 
     @IBOutlet weak var myLabel: UILabel!
     
