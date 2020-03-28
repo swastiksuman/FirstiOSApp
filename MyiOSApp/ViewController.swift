6b6794ecaefec426ef14aade3380921a65dea508
Swastik Panda <swastiksuman@gmail.com>
2020-03-28 19:37:19 +0530

    added button text change

--- MyiOSApp/ViewController.swift
+++ MyiOSApp/ViewController.swift
@@ -10,17 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var modeSwitcher: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func enableDarkMode(_ sender: Any) {
+        let subViews = view.subviews
         if(view.backgroundColor == UIColor.systemBackground){
                 view.backgroundColor = UIColor.darkGray
-                
+            for item in subViews{
+                if(item is UILabel){
+                    let thisLabel = item as! UILabel
+                    thisLabel.textColor = UIColor.white
+                }
+            }
+            modeSwitcher.setTitle("Light Mode", for: .normal)
         }else{
         view.backgroundColor = UIColor.systemBackground
+            for item in subViews{
+                if(item is UILabel){
+                    let thisLabel = item as! UILabel
+                    thisLabel.textColor = UIColor.black
+                }
+            }
+            modeSwitcher.setTitle("Dark Mode", for: .normal)
         }
     }
     
